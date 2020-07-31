712b683cb0fe855dfa1fecddd74fd1fb44aa4d57
kgignatyev <kgignatyev@gmail.com>
2020-07-31 09:45:23 -0700

    added use of requests package;

--- src/lambda1/main.py
+++ src/lambda1/main.py
@@ -1,10 +1,13 @@
 from dateutil.easter import easter
 import json
-
+import requests
 
 def main():
     # this should print "1984-04-22"
     result = easter(1984).isoformat()
+    res = requests.get('https://api.github.com')
+    status_code = res.status_code
+    result = "Request status code is [" + str(status_code)+ "] and easter is on " + result
     print(result)
     return json.dumps(result)
 
